20e145061a0dd1da7afe80be042df4bdcdc9ae1e
NickNedilko <overlord311089@gmail.com>
2023-02-26 11:47:10 +0200

    fix some problem

--- src/components/FriendList/Friend.styled.jsx
+++ src/components/FriendList/Friend.styled.jsx
@@ -1,36 +1,35 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
-export const List = styled.ul`
-` 
+export const List = styled.ul``;
 
 export const Friends = styled.li`
-display: flex;
-flex-direction: row-reverse;
-justify-content: space-around;
-margin: 0 auto;
-width: 200px;
-height: 60px;
-border: 1px solid black;
-align-items: center;
-margin-bottom: 15px;
-background-color: turquoise;
-border-radius: 6px;
-gap: 15px;
-box-shadow: 10px 5px 5px black;
-`
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-around;
+  margin: 0 auto;
+  width: 200px;
+  height: 60px;
+  border: 1px solid black;
+  align-items: center;
+  margin-bottom: 15px;
+  background-color: turquoise;
+  border-radius: 6px;
+  gap: 15px;
+  box-shadow: 10px 5px 5px black;
+`;
 export const FriendName = styled.span`
-font-size: 18px;
-font-weight: 600;
-`
+  font-size: 18px;
+  font-weight: 600;
+`;
 
-export const Avatar = styled.img`
-
-`
+export const Avatar = styled.img``;
 
 export const Status = styled.p`
-margin-left: 15px;
-width: 20px;
-height: 20px;
-border-radius: 50%;
-background-color: green;
-`
+  margin-left: 15px;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${props => {
+    return props.isOnline ? 'green' : 'red';
+  }};
+`;
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,26 +1,29 @@
 import { PropTypes } from 'prop-types';
 import { List, Friends, FriendName, Avatar, Status } from './Friend.styled';
 
-
-function FriendList({friends}){
-    return <List>
-  {friends.map(({id, name, avatar, isOnline})=>
-    <Friends key={id}>
-    <FriendName>{name}</FriendName>
-    <Avatar src={avatar} alt={name} width="48" />
-    <Status></Status>
-  </Friends>)}
+function FriendList({ friends }) {
+  return (
+    <List>
+      {friends.map(({ id, name, avatar, isOnline }) => (
+        <Friends key={id}>
+          <FriendName>{name}</FriendName>
+          <Avatar src={avatar} alt={name} width="48" />
+          <Status isOnline={isOnline}></Status>
+        </Friends>
+      ))}
     </List>
+  );
 }
 
 FriendList.propTypes = {
-  friends: PropTypes.arrayOf(PropTypes.exact({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired, 
-    isOnline: PropTypes.bool,
-  }))
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool,
+    })
+  ),
 };
 
-
-export default FriendList 
+export default FriendList;
